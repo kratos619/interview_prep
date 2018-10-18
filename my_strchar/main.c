c6e8012f0dc4b91c0032f1f4e6bf5b5fae7b2375
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-18 13:02:34 +0530

    creat own strlen

--- my_strchar/main.c
+++ my_strchar/main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    my_strchar(const char *str, int c){
+    if(str == NULL){
+        return 0;
+    }
+
+    while(*str != '\0'){
+        if(*str == c){
+            return (char*) str;
+        }
+        str++
+    }
+    return NULL;
+
+    }
+    //printf("Hello world!\n");
     return 0;
 }
