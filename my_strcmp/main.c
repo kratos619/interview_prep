cfba5f54b8dc70ce3543043af5bf9f7a71b5c9d8
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-18 17:07:34 +0530

    Create own strcmp function

--- my_strcmp/main.c
+++ my_strcmp/main.c
@@ -3,6 +3,33 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int my_strcmp (const char* str1, const char* str2){
+        if(str1 == NULL && str2 == NULL ){
+            return 0;
+        }
+
+        const char *ch1  = str1;
+        const char *ch2  = str2;
+
+
+
+        while( *ch1 == '\0' && *ch2 == '\0'){
+            if(*ch1 == *ch2){
+                    printf(ch1);
+            printf(ch2);
+                return 1;
+            }else{
+                return 0;
+            }
+            ch1++;
+            ch2++;
+        }
+    }
+
+
+    int result = my_strcmp("gaurav", "gaurav");
+     printf("my_strcmp(str1, str2) = %d\n", result);
+
+
     return 0;
 }
