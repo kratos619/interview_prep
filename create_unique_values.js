77be2134eb230abf394c55e3d18b39b4487c65f3
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2019-04-05 01:11:22 +0530

    New Program base on genrators

--- create_unique_values.js
+++ create_unique_values.js
@@ -12,5 +12,36 @@ createUniqueValue(dataArrayStrings)
 Output
 [ 12, 212, 122, 2, 22, 11, 1, 14, 44, 4, 9, 10 ]
 [ 'a', 'b', 'c', 'gaurav', 'saurav' ]
-/*
+*/
+
+// New Program base on genrators
+
+function hi() {
+    return 'hi'
+}
+function hello() {
+    return 'hello'
+}
+function grettings(name) {
+    return `hello ${name}`
+}
+
+function *controllerFun() {
+    yield hi();
+    yield hello();
+    yield grettings("gaurav",);
+    yield "aasaasd"
+}
+
+const p1 = controllerFun()
+const newArray = [];
+console.log(p1.next())
+console.log(p1.next())
+console.log(p1.next())
+console.log(p1.next())
+
+for (let item of controllerFun()) {
+  newArray.push(item)
+}
+newArray
 
