3008427859fc9076e8b29f0daa55081fc7ddbd0f
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-13 22:38:37 +0530

    :fire:  sort by length

--- JS/arr.sort().js
+++ JS/arr.sort().js
@@ -2,6 +2,8 @@ let a = [6, 4, 15, 10];
 let b = ["b", "a", "d", "c"];
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
+var strings = ["aa", "aaaa", "aaa", "a"];
+
 //ascending order
 function numAscendingOrder(num1, num2) {
   return num1 - num2;
@@ -10,6 +12,13 @@ function numAscendingOrder(num1, num2) {
 function numDescendingOrder(num1, num2) {
   return num2 - num1;
 }
+
+//function sort by length
+function sortByLength(str1, str2) {
+  return str1.length - str2.length;
+}
+
+console.log(strings.sort(sortByLength));
 console.log(b.sort());
 console.log(b.reverse());
 console.log(fruits.sort());
