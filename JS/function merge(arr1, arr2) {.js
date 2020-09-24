b31efb515b0bf60c20200ced4f394e8ebc22707b
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-24 11:22:19 +0530

    Create function merge(arr1, arr2) {.js

---
+++ JS/function merge(arr1, arr2) {.js
@@ -0,0 +1,25 @@
+function merge(arr1, arr2) {
+  let arr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      arr.push(arr1[i]);
+      i++;
+    } else {
+      arr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    arr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    arr.push(arr2[j]);
+    j++;
+  }
+  return arr;
+}
+
+console.log(merge([2, 14, 99, 100], [1, 10, 50]));
